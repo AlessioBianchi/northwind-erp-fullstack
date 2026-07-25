4b67706d8690d666c53bb550e11d36d1a0aa5ab5
AlessioBianchi <balessio15@gmail.com>
2026-07-25 17:24:15 +0200

    Added environment.ts and api-base-url.token.ts to expose base url

--- angular-erp/src/app/service/dashboard.service.ts
+++ angular-erp/src/app/service/dashboard.service.ts
@@ -2,13 +2,14 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { DashboardData } from '../layout/dashboard/dashboard.model';
+import { API_BASE_URL } from '../api-base-url.token';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
-  private http = inject(HttpClient); 
-  private apiUrl = 'http://localhost:8080/api/v1/dashboard/stats';
+  private http = inject(HttpClient);
+  private apiUrl = `${inject(API_BASE_URL)}/api/v1/dashboard/stats`;
 
   getDashboardStats(): Observable<DashboardData> {
     return this.http.get<DashboardData>(this.apiUrl, { 
